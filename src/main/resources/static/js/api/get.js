14660ad388d5d13d49ba087751bbccf014ec6e53
montlein <montvlein@gmail.com>
2022-07-05 11:29:15 -0400

    los metodos vuelven una tabla con los datos

--- src/main/resources/static/js/api/get.js
+++ src/main/resources/static/js/api/get.js
@@ -1,26 +1,94 @@
-function listarPacientes() {
+function getPacientes() {
     return fetch("v1/paciente/all")
     .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        return data
+    .then(lista => {
+        let cargarFilaPacientes = ""
+        for (let pacientes of lista){
+        cargarFilaPacientes += `<tr>
+                                        <th scope="row">${pacientes.id}</th>
+                                        <td>${pacientes.nombre}</td>
+                                        <td>${pacientes.apellido}</td>
+                                        <td>${pacientes.dni}</td>
+                                        <td>${pacientes.domicilio}</td>
+                                        <td><button class="btn btn-primary me-1">Actualizar</button><button class="btn btn-primary">Eliminar</button></td>
+                                  </tr>`
+        }
+        return  `<table class="table table-hover">
+        <thead>
+        <tr>
+          <th scope="col">Id</th>
+          <th scope="col">Nombre</th>
+          <th scope="col">Apellido</th>
+          <th scope="col">DNI</th>
+          <th scope="col">Domicilio</th>
+          <th scope="col">Acción</th>
+        </tr>
+        </thead>
+        <tbody>
+        ${cargarFilaPacientes}
+        </tbody>
+      </table>`
         })
 }
 
-function listarOdontologos() {
+function getOdontologos() {
     return fetch("v1/odontologo/all")
     .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        return data
+    .then(lista => {
+        let cargarFilaOdontologo = "";
+        for (let odontologo of lista){
+        cargarFilaOdontologo += `<tr>
+                                        <th scope="row">${odontologo.id}</th>
+                                        <td>${odontologo.nombre}</td>
+                                        <td>${odontologo.apellido}</td>
+                                        <td>${odontologo.matricula}</td>
+                                        <td><button class="btn btn-primary me-1">Actualizar</button><button class="btn btn-primary">Eliminar</button></td>
+                                  </tr>`
+        }
+        return  `<table class="table table-hover">
+        <thead>
+        <tr>
+          <th scope="col">Id</th>
+          <th scope="col">Nombre</th>
+          <th scope="col">Apellido</th>
+          <th scope="col">Matrícula</th>
+          <th scope="col">Acción</th>
+        </tr>
+        </thead>
+        <tbody>
+            ${cargarFilaOdontologo}
+        </tbody>
+      </table>`
         })
 }
 
-function listarTurnos() {
+function getTurnos() {
     return fetch("v1/turno/all")
     .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        return data
-        })
+    .then(lista => {
+        let cargarFilaTurno = "";
+        for (let turno of lista){
+        cargarFilaTurno += `<tr>
+                                        <th scope="row">${turno.id}</th>
+                                        <td>${turno.fecha}</td>
+                                        <td>${turno.paciente}</td>
+                                        <td>${turno.odontologo}</td>
+                                        <td><button class="btn btn-primary me-1">Actualizar</button><button class="btn btn-primary">Eliminar</button></td>
+                                  </tr>`
+        }
+        return  `<table class="table table-hover">
+        <thead>
+        <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Fecha</th>
+            <th scope="col">Paciente</th>
+            <th scope="col">Odontologo</th>
+            <th scope="col">Acción</th>
+        </tr>
+        </thead>
+        <tbody>
+            ${cargarFilaTurno}
+        </tbody>
+      </table>`
+    })
 }
