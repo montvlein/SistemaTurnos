e37de9fb48cde6570ac34808b8959fc289179331
montvlein <75510452+montvlein@users.noreply.github.com>
2022-07-04 23:49:18 -0400

    Add files via upload

---
+++ src/main/java/com/dh/proyecto/Models/dtos/PacienteDTO.java
@@ -0,0 +1,13 @@
+package com.dh.proyecto.Models.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class PacienteDTO {
+    private Long id;
+    private String apellido;
+    private String nombre;
+    private String dni;
+}
