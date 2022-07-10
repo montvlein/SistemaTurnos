faaf50ff01e6495a3439a4c2185ea40b8eae8d00
montvlein <montvlein@gmail.com>
2022-07-10 20:08:35 -0300

    se implementaron todos los test del CRUD

--- src/test/java/com/dh/proyecto/Services/implement/OdontologoServiceTest.java
+++ src/test/java/com/dh/proyecto/Services/implement/OdontologoServiceTest.java
@@ -20,26 +20,34 @@ public class OdontologoServiceTest {
     private Odontologo odontologo;
 
     @BeforeEach
-    public void cargarDataSet() {
+    public void cargarDataSet() throws Exception {
         odontologo = new Odontologo();
+        odontologo.setMatricula("1234");
+        services.guardar(odontologo);
     }
 
     @Test
     public void buscar() {
+        Odontologo o = services.buscar(1L);
+        Assertions.assertEquals("1234", o.getMatricula());
     }
 
     @Test
     public void guardarOdontologoEnDB() throws Exception {
-        services.guardar(odontologo);
-        Odontologo o = services.buscar(1L);
+        services.guardar(new Odontologo());
+        Odontologo o = services.buscar(2L);
         Assertions.assertNotNull(o);
     }
 
     @Test
     public void eliminar() {
+        services.eliminar(1L);
+        Odontologo o = services.buscar(1L);
+        Assertions.assertNull(o);
     }
 
     @Test
     public void listarTodos() {
+        Assertions.assertNotEquals(0, services.listarTodos().size());
     }
 }
