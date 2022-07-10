e9cc019bbf1c76662af1275c1a67823ffc40b4ae
montvlein <montvlein@gmail.com>
2022-07-10 18:32:34 -0300

    eliminando mapper

--- src/main/java/com/dh/proyecto/Controller/TurnoController.java
+++ src/main/java/com/dh/proyecto/Controller/TurnoController.java
@@ -6,7 +6,6 @@ import com.dh.proyecto.Models.entities.Odontologo;
 import com.dh.proyecto.Models.entities.Paciente;
 import com.dh.proyecto.Models.entities.Turno;
 import com.dh.proyecto.Services.iServices;
-import com.dh.proyecto.Util.ModelMapperConfig;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
