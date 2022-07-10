7962dab8a958e2a3c5d43cf6c4094177249d393f
montvlein <montvlein@gmail.com>
2022-07-10 18:50:56 -0300

    se agrego un logger al generar una exception

--- src/main/java/com/dh/proyecto/Exceptions/GlobalExceptions.java
+++ src/main/java/com/dh/proyecto/Exceptions/GlobalExceptions.java
@@ -1,5 +1,6 @@
 package com.dh.proyecto.Exceptions;
 
+import org.apache.log4j.Logger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -7,8 +8,10 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptions extends Exception{
 
+    private static final Logger logger = Logger.getLogger(GlobalExceptions.class);
     @ExceptionHandler({BadRequestException.class})
     public ResponseEntity<String> manejarError(BadRequestException ex){
+        logger.error(ex.getMessage());
         return ResponseEntity.badRequest().body(ex.getMessage());
     }
 
