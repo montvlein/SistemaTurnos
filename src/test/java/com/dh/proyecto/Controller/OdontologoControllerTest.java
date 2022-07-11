a7c96aa57237396a79a87bceb8ff4d5827c18c60
montlein <montvlein@gmail.com>
2022-07-10 23:38:57 -0400

    se elimino importacion sin uso

--- src/test/java/com/dh/proyecto/Controller/OdontologoControllerTest.java
+++ src/test/java/com/dh/proyecto/Controller/OdontologoControllerTest.java
@@ -18,7 +18,6 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
