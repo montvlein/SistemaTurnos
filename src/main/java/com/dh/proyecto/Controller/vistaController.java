eb8139951480f6fbf02f0ccb96dee9486faabe0c
montlein <montvlein@gmail.com>
2022-07-12 01:54:15 -0400

    se genero vista para renderizar webs segun rol

--- src/main/java/com/dh/proyecto/Controller/vistaController.java
+++ src/main/java/com/dh/proyecto/Controller/vistaController.java
@@ -1,12 +1,21 @@
 package com.dh.proyecto.Controller;
 
+import org.apache.log4j.Logger;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @Controller
 @RequestMapping
 public class vistaController {
+
+    private static final Logger logger = Logger.getLogger(vistaController.class);
 
     @GetMapping
     public String renderIndex() {
@@ -21,5 +30,20 @@ public class vistaController {
     @GetMapping("admin")
     public String renderAdmin() {
         return "admin";
+    }
+
+    @GetMapping("success")
+    public String rederPorRol(Authentication authResult) {
+        String rol = authResult.getAuthorities().toString();
+        String render;
+        logger.info(rol);
+        if (rol.contains("ADMIN")) {
+            render = "admin";
+        } else if (rol.contains("USER")) {
+            render = "user";
+        } else {
+            render = "/";
+        }
+        return render;
     }
 }
